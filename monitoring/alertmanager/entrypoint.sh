9257d3ab493f5649fbfd35dbee44bc19f96fa8de
pianosuki <pianosuki@protonmail.com>
2026-07-09 10:34:41 -0300

    simplify monitoring stack: stock images, add exporters, fix alerts

--- monitoring/alertmanager/entrypoint.sh
+++ monitoring/alertmanager/entrypoint.sh
@@ -2,14 +2,14 @@
 set -e
 
 CONFIG_FILE="/etc/alertmanager/alertmanager.yml"
-NULL_CONFIG="/etc/alertmanager/alertmanager-null.yml"
+BLACKHOLE_CONFIG="/etc/alertmanager/alertmanager-blackhole.yml"
 RENDERED="/tmp/alertmanager.rendered.yml"
 
 if [ -n "${ALERTMANAGER_DISCORD_WEBHOOK_URL:-}" ]; then
   cp "$CONFIG_FILE" "$RENDERED"
   sed -i "s|\${ALERTMANAGER_DISCORD_WEBHOOK_URL}|${ALERTMANAGER_DISCORD_WEBHOOK_URL}|g" "$RENDERED"
 else
-  cp "$NULL_CONFIG" "$RENDERED"
+  cp "$BLACKHOLE_CONFIG" "$RENDERED"
 fi
 
 exec /bin/alertmanager --config.file="$RENDERED" "$@"
